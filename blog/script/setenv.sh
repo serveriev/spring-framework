0dde65f762d61e62fb92bf2a068637b54496abb3
lenur <lenur.serveriev@gmail.com>
2020-06-09 11:35:44 +0300

    blog: added logger

--- blog/script/setenv.sh
+++ blog/script/setenv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export JAVA_OPTS="-DAPP_BLOG_DB_USER={db_user} -DAPP_BLOG_DB_PASSWORD={db_pass} -DAPP_BLOG_DB_HOST={db_host} -DAPP_BLOG_DB_PORT={db_port} -DAPP_BLOG_DB_NAME={db_name}"
+export JAVA_OPTS="-DAPP_BLOG_PATH={path_to_project} -DAPP_BLOG_DB_USER={db_user} -DAPP_BLOG_DB_PASSWORD={db_pass} -DAPP_BLOG_DB_HOST={db_host} -DAPP_BLOG_DB_PORT={db_port} -DAPP_BLOG_DB_NAME={db_name}"
