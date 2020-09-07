39f30f7adf0263b859b1fc8b4c7774a0424ae116
lenur <lenur.serveriev@gmail.com>
2020-09-07 12:23:23 +0300

    spring boot guide: added context load test

--- spring-guides/gs-validating-form-input/src/test/java/io/lenur/spring/guides/validatingforminput/ValidatingFormInputApplicationTests.java
+++ spring-guides/gs-validating-form-input/src/test/java/io/lenur/spring/guides/validatingforminput/ValidatingFormInputApplicationTests.java
@@ -1,13 +1,18 @@
 package io.lenur.spring.guides.validatingforminput;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ValidatingFormInputApplicationTests {
+    @Autowired
+    private WebController webController;
 
     @Test
     void contextLoads() {
+        assertThat(webController).isNotNull();
     }
-
 }
